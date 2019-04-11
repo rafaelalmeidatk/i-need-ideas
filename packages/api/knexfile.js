bf5477614d9553b543b2ba276c3f1998b5a1072d
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-04-11 04:47:41 -0300

    Use DATABASE_URL env variable if available to connect to the database

--- packages/api/knexfile.js
+++ packages/api/knexfile.js
@@ -1,6 +1,9 @@
 const path = require('path');
 require('dotenv').config();
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const IS_DEV = NODE_ENV === 'development';
+
 const migrations = {
   tableName: 'knex_migrations',
   directory: path.normalize(path.join(__dirname, 'src/db/migrations'))
@@ -10,19 +13,21 @@ const seeds = {
   directory: path.normalize(path.join(__dirname, `src/db/seeds`))
 };
 
+const connection = process.env.DATABASE_URL || {
+  host: '127.0.0.1',
+  user: process.env.DEV_POSTGRES_USER,
+  password: process.env.DEV_POSTGRES_PASS,
+  database: process.env.DEV_POSTGRES_DB
+};
+
 module.exports = {
   client: 'postgresql',
-  connection: {
-    host: '127.0.0.1',
-    user: process.env.DEV_POSTGRES_USER,
-    password: process.env.DEV_POSTGRES_PASS,
-    database: process.env.DEV_POSTGRES_DB
-  },
+  connection,
   pool: {
     min: 2,
     max: 10
   },
-  debug: true,
+  debug: IS_DEV,
   migrations,
   seeds
 };
