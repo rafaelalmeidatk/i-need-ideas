a3f52bc9d685fd04779714f0c09ec9f2a35df357
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-04-11 03:32:51 -0300

    Read front-end url from .env, add .env-sample files

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: [process.env.FRONTEND_URL],
     methods: ['GET', 'POST'],
     credentials: true
   })
