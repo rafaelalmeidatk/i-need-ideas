bffacbf8246358a8a01cc97e217bd0955a131432
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-04-11 06:02:43 -0300

    Set CORS rules without cors method

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -10,13 +10,30 @@ import routes from './routes';
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: ['https://i-need-ideas.herokuapp.com/'],
-    // methods: ['GET', 'POST'],
-    credentials: true
-  })
-);
+// app.use(
+//   cors({
+//     origin: ['https://i-need-ideas.herokuapp.com/'],
+//     methods: ['GET', 'POST'],
+//     credentials: true
+//   })
+// );
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  console.log('the origin is ' + req.headers.origin);
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+  );
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 //--------------------
 // Session
