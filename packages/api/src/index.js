996fd36706b1d3c046994bf878e94beb23de8fb6
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-04-21 17:23:14 -0300

    Trust proxy, add secure flag to cookies

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -3,13 +3,18 @@ import session from 'express-session';
 import bodyParser from 'body-parser';
 import routes from './routes';
 
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
 //--------------------
 // Server startup
 
 const app = express();
+app.set('trust proxy', 1);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
+  // This should not be in an actual production app, it is basically
+  // a hack to allow wildcard origin with credentials on
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   res.header('Access-Control-Allow-Credentials', true);
@@ -35,6 +40,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       httpOnly: true,
+      secure: IS_PRODUCTION,
     },
   })
 );
